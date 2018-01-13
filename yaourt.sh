ddf4be8d424cd92267fded95c0140ea1f6c0a571
IDerr <ibrahim@derraz.fr>
2018-01-13 17:21:18 +0100

    adding stow

--- yaourt.sh
+++ yaourt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yaourt -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4
+yaourt -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4 stow
