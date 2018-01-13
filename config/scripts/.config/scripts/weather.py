d31c3f7665c3f45bcafde33424bb559035627416
Skyforce77 <usulguru@gmail.com>
2018-01-13 23:32:27 +0100

    Removed useless weather text

--- config/scripts/.config/scripts/weather.py
+++ config/scripts/.config/scripts/weather.py
@@ -17,14 +17,13 @@ icons = {
     '11n' : '',
     '13d' : ' ',
     '13n' : ' ',
-    '50d' : '',
-    '50n' : ''
+    '50d' : '',
+    '50n' : ''
 }
 owm = pyowm.OWM('')
 weather = owm.weather_at_place('Paris,FR').get_weather()
-print("{0}{1}, {2:0.1f}°C".format(
+print("{0} {1:0.1f}°C".format(
         icons[weather.get_weather_icon_name()],
-        weather.get_status(),
         float(weather.get_temperature(unit='celsius')['temp'])
     )
 )
