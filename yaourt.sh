ab82f801a6ef3468334e65d79cba75f77380b298
⌨ Skyforce77 <usulguru@gmail.com>
2018-01-14 16:11:26 +0100

    Update yaourt.sh

--- yaourt.sh
+++ yaourt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yaourt --needed -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4 stow imagemagick scrot xautolock ncmpcpp 
+yaourt --needed -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4 stow imagemagick scrot xautolock ncmpcpp python-pyowm
 
