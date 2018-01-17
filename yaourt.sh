444f309547f27858aed9b263caddd19d639cc024
Skyforce77 <usulguru@gmail.com>
2018-01-17 20:24:28 +0100

    Added dunst config

--- yaourt.sh
+++ yaourt.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-yaourt --needed -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4 stow imagemagick scrot xautolock ncmpcpp python-pyowm
-
+yaourt --needed -S rofi i3-gaps polybar terminator compton feh adobe-source-code-pro-fonts otf-font-awesome-4 stow imagemagick scrot xautolock ncmpcpp python-pyowm dunst
